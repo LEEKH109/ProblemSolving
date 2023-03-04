f7ba14e421572b6f2329c9dda89037d506bc1e7b
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-04 15:24:54 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,11 +1,18 @@
 function solution(n) {
-    let max = Math.floor(n/2);
+    // 두번째 풇이
     let answer = [];
-    for(i=0; i<=max ; i++){
-        let el =2*i+1;
-        if(el <= n){
-        answer.push(el);
-        }
+    for(i=1;i<=n;i+=2){
+        answer.push(i)
     }
     return answer;
+    // 첫번째 풀이
+    // let max = Math.floor(n/2);
+    // let answer = [];
+    // for(i=0; i<=max ; i++){
+    //     let el =2*i+1;
+    //     if(el <= n){
+    //     answer.push(el);
+    //     }
+    // }
+    // return answer;
 }
