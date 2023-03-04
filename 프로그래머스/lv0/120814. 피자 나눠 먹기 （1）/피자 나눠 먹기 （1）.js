bf122edcf1b28b2f7caa29680b58b8b53c3c2945
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-04 16:04:19 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,4 +1,9 @@
 function solution(n) {
-    let pizza = Math.ceil(n/7)
-    return pizza;
+    // 첫번째 풀이
+    // let pizza = Math.ceil(n/7)
+    // return pizza;
+    // 두번째 풀이 => Math 함수 없이
+    let pizza = 0;
+    (n%7 !==0) ? pizza = parseInt(n/7)+1 : pizza = parseInt(n/7)
+    return pizza
 }
