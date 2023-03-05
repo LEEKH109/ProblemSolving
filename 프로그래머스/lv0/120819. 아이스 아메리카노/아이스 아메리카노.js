864ff8ceae0b5e4002c32fd6370c053922fd663f
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-05 18:33:52 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,9 @@
 function solution(money) {
-    let answer = [];
-    answer.push(parseInt(money/5500));
-    answer.push(parseInt(money%5500))
-    return answer;
+    // 첫 번째 방식
+    // let answer = [];
+    // answer.push(parseInt(money/5500));
+    // answer.push(parseInt(money%5500))
+    // return answer;
+    // 두 번째 방식
+    return [parseInt(money/5500), money%5500]
 }
