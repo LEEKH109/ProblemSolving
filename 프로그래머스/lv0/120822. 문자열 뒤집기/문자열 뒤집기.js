e33385fd381aa856186f517be71466e35837d227
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-06 12:05:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,14 +1,17 @@
-function solution(my_string) {
-    return my_string.split('').reverse().join('');
-}
+// 간단한 풀이 => 이게 더 빠름
 // function solution(my_string) {
-//     let reverse_string =[];
-//      for(el of my_string){
-//         reverse_string.unshift(el)
-//     }
-//     let answer = '';
-//     for(el of reverse_string){
-//         answer=answer+el
-//     }
-//     return answer;
+//     return my_string.split('').reverse().join('');
+    
 // }
+// 첫번째 풀이
+function solution(my_string) {
+    let reverse_string =[];
+     for(el of my_string){
+        reverse_string.unshift(el)
+    }
+    let answer = '';
+    for(el of reverse_string){
+        answer=answer+el
+    }
+    return answer;
+}
