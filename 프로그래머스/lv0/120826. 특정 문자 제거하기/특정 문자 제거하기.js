7747dd72db532d00a63aaef6b1406aa00a33d0cd
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-06 12:59:29 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,7 +1,10 @@
 function solution(my_string, letter) {
-    let answer = '';
-    for(el of my_string){
-        if(el !== letter) answer = answer+el
-    }
-    return answer;
+    // 짧은 풀이1
+    return my_string.replaceAll(letter, "");
+    // 나의 풀이
+    // let answer = '';
+    // for(el of my_string){
+    //     if(el !== letter) answer = answer+el
+    // }
+    // return answer;
 }
