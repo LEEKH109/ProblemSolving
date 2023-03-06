ce3e728b6f330884f51fe053db90b0f3271598ea
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-06 20:44:17 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
@@ -1,7 +1,10 @@
 function solution(arr) {
-    let answer = 0;
-    for(el of arr){
-        answer+=el
-    }
-    return answer/arr.length;
+    // 첫번째 풀이
+    // let answer = 0;
+    // for(el of arr){
+    //     answer+=el
+    // }
+    // return answer/arr.length;
+    // 두번째 풀이
+    return arr.reduce((a,b)=>a+b)/arr.length
 }
