440740da170b0ab2bdaa817658cb428ee7764dad
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-07 11:32:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,8 +1,13 @@
 function solution(num_list) {
-    let odd = 0;
-    let even =0;
-    for(el of num_list){
-        el%2===0 ? even++ : odd++
-    }
-    return   [even, odd];
+    // 첫번째 풀이
+    // let odd = 0;
+    // let even =0;
+    // for(el of num_list){
+    //     el%2===0 ? even++ : odd++
+    // }
+    // return [even, odd];
+    // arr.filter 사용
+    return [num_list.filter(el => el%2 === 0).length, num_list.filter(el => el%2 === 1).length];
+    // reduce 사용
+    // return list.reduce((acc, cur) => (cur & 1 ? acc[1]++ : acc[0]++, acc), [0, 0])
 }
