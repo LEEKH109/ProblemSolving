b826a2f8f37d6925060ffce4838f551705b40a33
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-07 12:44:40 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,9 +1,12 @@
 function solution(numbers) {
     //가장 큰수 순서로 정렬을 해서 처리를 하면 어떨까?
-    numbers.sort(function(a, b)  {
-        if(a < b) return 1;
-        if(a === b) return 0;
-        if(a > b) return -1;
-        });
+    // sort 함수 입력 방식 1번
+    // numbers.sort(function(a, b)  {
+    //     if(a < b) return 1;
+    //     if(a === b) return 0;
+    //     if(a > b) return -1;
+    //     });
+    // sort 함수 입력 방식 2번 => 간단
+    numbers.sort((a,b)=>b-a)
     return numbers[0]*numbers[1];
 }
