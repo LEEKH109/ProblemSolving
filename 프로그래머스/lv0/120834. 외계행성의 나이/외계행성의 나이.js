9a192b965211b18f2b6de18c209e5b29fb13e50b
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-08 01:01:26 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,28 @@
+function solution(age) {
+    // 첫 번째 풀이
+    // let answer = '';
+    // let string = String(age);
+    // for(el of string){
+    //     if(el !== undefined){
+    //         if(el === '0') answer+='a'
+    //         if(el === '1') answer+='b'
+    //         if(el === '2') answer+='c'
+    //         if(el === '3') answer+='d'
+    //         if(el === '4') answer+='e'
+    //         if(el === '5') answer+='f'
+    //         if(el === '6') answer+='g'
+    //         if(el === '7') answer+='h'
+    //         if(el === '8') answer+='i'
+    //         if(el === '9') answer+='j'
+    //     }
+    // }
+    // return answer;
+    // 두 번쨰 풀이
+    let rule = ['a','b','c','d','e','f','g','h','i','j']
+    let answer = '';
+    let string = String(age);
+    for(el of string){
+        answer+=rule[el]
+    }
+    return answer
+}
