14adde04bbf68c749614829c67c01896de4489ad
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-08 20:35:07 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,7 +1,17 @@
 function solution(n) {
+    // 첫 번째 풀이
     let answer = 0;
-    for(i=1;i<=n;i++){
-        if(n%i === 0) answer+=i
+    // for(i=1;i<=n;i++){
+    //     if(n%i === 0) answer+=i
+    // }
+    // return answer;
+    // 참신했던 풀이
+    let i;
+    for (i = 1; i <= Math.sqrt(n); i++){
+        if (!(n%i)) {
+            answer += (i+n/i);
+        }
     }
-    return answer;
+    i--;
+    return (i === n/i) ? answer-i : answer;
 }
