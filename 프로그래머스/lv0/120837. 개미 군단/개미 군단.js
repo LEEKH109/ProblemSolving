c3aeef68bb5a69f978e23d7c1cf296dba90611d2
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-08 14:58:05 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,9 @@
 function solution(hp) {
     // 첫 번째 풀이
-    let antg = parseInt(hp/5)
-    let ants = parseInt((hp-5*antg)/3)
-    let antw = hp-5*antg-3*ants
-    return antg+ants+antw;
+    // let antg = parseInt(hp/5)
+    // let ants = parseInt((hp-5*antg)/3)
+    // let antw = hp-5*antg-3*ants
+    // return antg+ants+antw;
+    // 두 번째 풀이
+    return parseInt(hp/5)+parseInt((hp%5)/3)+(hp%5)%3;
 }
