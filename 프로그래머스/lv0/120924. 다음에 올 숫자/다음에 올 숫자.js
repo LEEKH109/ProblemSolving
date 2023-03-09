fe7b884f50cba2cb60db421c5fff022f1b301ec6
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-09 23:29:44 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,12 @@
 function solution(common) {
+    // 첫 번째 풀이
+    // if(common[2] - common[1] === common[1] - common[0]){
+    //     return common[0]+(common[1]-common[0])*(common.length)
+    // }else{
+    //     return common[0]*Math.pow(common[1]/common[0], common.length)
+    // }
+    // 두 번째 풀이 => 마지막 요소에서 처리
     if(common[2] - common[1] === common[1] - common[0]){
-        return common[0]+(common[1]-common[0])*(common.length)
-    }else{
-        return common[0]*Math.pow(common[1]/common[0], common.length)
-    }
-    
+        return common[common.length-1]+common[1] - common[0]
+    } return common[common.length-1]*(common[1]/common[0])
 }
