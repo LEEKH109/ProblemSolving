c665fa5f0f3017ed100ced396879c35dd1980e07
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-09 18:06:18 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,4 +1,7 @@
 function solution(arr) {
-    let newArr = [...arr].sort((a,b)=>a-b)
-    return arr.length === 1 ? [-1] : arr.filter((el)=>el>newArr[0])
+    // 첫 번째 풀이
+    // let newArr = [...arr].sort((a,b)=>a-b)
+    // return arr.length === 1 ? [-1] : arr.filter((el)=>el>newArr[0])
+    // 두 번째 풀이
+    return arr.length === 1 ? [-1] : arr.filter((el)=>el>Math.min(...arr))
 }
