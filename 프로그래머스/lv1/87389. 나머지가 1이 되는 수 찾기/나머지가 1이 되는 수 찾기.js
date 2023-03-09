4cce0af5042c1ae142be9b578f6d17b7479b88e9
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-09 17:35:04 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    // for문 으로 간단하게 풀기
+    let answer = 0;
+    for(i=1;i<=n;i++){
+        if(n%i === 1){
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
