bb2762587614f0b6a8d349055a4c6353fc801d91
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-09 23:32:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -6,7 +6,9 @@ function solution(common) {
     //     return common[0]*Math.pow(common[1]/common[0], common.length)
     // }
     // 두 번째 풀이 => 마지막 요소에서 처리
-    if(common[2] - common[1] === common[1] - common[0]){
-        return common[common.length-1]+common[1] - common[0]
-    } return common[common.length-1]*(common[1]/common[0])
+    // if(common[2] - common[1] === common[1] - common[0]){
+    //     return common[common.length-1]+common[1] - common[0]
+    // } return common[common.length-1]*(common[1]/common[0])
+    // 세 번째 풀이 => 삼항 연산자로 => 연산 속도가 차이 나는지 비교 => 가독성은 if 문 대비 많이 떨어진다
+    return common[2] - common[1] === common[1] - common[0] ? common[common.length-1]+common[1] - common[0] : common[common.length-1]*(common[1]/common[0])
 }
