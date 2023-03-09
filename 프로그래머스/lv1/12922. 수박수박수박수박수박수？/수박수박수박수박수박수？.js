b9e3c1fa3e436f512d9cc445ada1d3a7ecbd6657
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-09 17:14:37 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,10 @@
 function solution(n) {
     // 간단하게 푸는 방법
-    return n%2 === 1 ? '수박'.repeat(parseInt(n/2))+'수' : '수박'.repeat(n/2)
+    // return n%2 === 1 ? '수박'.repeat(parseInt(n/2))+'수' : '수박'.repeat(n/2)
     // 확장성 있게 푸는 방법
-    // let answer =''
-    // for(i=1;i<=n;i++){
-    //     i%2 ? answer+='수' : answer+='박'
-    // }
-    // return answer
+    let answer =''
+    for(i=1;i<=n;i++){
+        i%2 ? answer+='수' : answer+='박'
+    }
+    return answer
 }
