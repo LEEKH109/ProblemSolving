9fd59ee65a1d60d3c0ea791e76d7af151ed5a923
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-10 11:47:08 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,8 +1,11 @@
 function solution(chicken) {
-    let service = 0;
-    while(chicken > 9){
-        service += parseInt(chicken/10)
-        chicken = chicken%10+parseInt(chicken/10)
-    }
-    return service
+    // 첫 번째 플이 => 확장성 있는 풀이
+    // let service = 0;
+    // while(chicken > 9){
+    //     service += parseInt(chicken/10)
+    //     chicken = chicken%10+parseInt(chicken/10)
+    // }
+    // return service
+    // 두 번째 풀이 => 최적화 => 재귀
+    return ~~(chicken*0.1111111)
 }
