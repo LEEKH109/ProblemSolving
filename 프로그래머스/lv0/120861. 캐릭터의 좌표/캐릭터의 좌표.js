36b34290f1e4f4ef14679c6a2cd4507c4b4a447d
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-10 15:12:48 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,10 +1,10 @@
 function solution(keyinput, board) {
     let x=0, y=0;
     // for(el of keyinput){
-    //     if(el === 'up' && Math.abs(y)<(board[1]/2) ) y++
-    //     if(el === 'down' && Math.abs(y)<(board[1]/2) ) y--
-    //     if(el === 'left' && Math.abs(x)<(board[0]/2) ) x--
-    //     if(el === 'right' && Math.abs(x)<(board[0]/2) ) x++                 
+    //     if(el === 'up' && Math.abs(y)<(board[1]/2)-1 ) y++
+    //     if(el === 'down' && Math.abs(y)<(board[1]/2)-1 ) y--
+    //     if(el === 'left' && Math.abs(x)<(board[0]/2)-1 ) x--
+    //     if(el === 'right' && Math.abs(x)<(board[0]/2)-1 ) x++                 
     // }
     for (el of keyinput) {
         if (el === "right" && x + 1 < (board[0] / 2)) x++ 
