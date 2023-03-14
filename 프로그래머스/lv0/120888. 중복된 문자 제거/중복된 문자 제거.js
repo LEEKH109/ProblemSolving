4a45a7464cf1239daab290121e326104d5e804d0
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-14 14:40:35 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,3 +1,6 @@
 function solution(my_string) {
-    return my_string.split('').filter((el1,el2)=>my_string.indexOf(el1) === el2).join('');
+    // 첫 번째 풀이
+    // return my_string.split('').filter((el1,el2)=>my_string.indexOf(el1) === el2).join('');
+    // 두 번째 풀이   
+    return [...new Set(my_string)].join('');
 }
