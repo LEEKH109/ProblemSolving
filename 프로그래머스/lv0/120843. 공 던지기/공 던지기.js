f1ee63a29e310675eef407d6f731e1286d85f3f1
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-22 14:37:12 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,5 +1,8 @@
 function solution(numbers, k) {
-    let result = (2*k-1) % numbers.length
-    if(result === 0) result = numbers[numbers.length-1]
-    return result
+    // 첫 번째 풀이
+    // let result = (2*k-1) % numbers.length
+    // if(result === 0) result = numbers[numbers.length-1]
+    // return result
+    // 두 번째 풀이
+     return numbers[((2 * (k -1))) % numbers.length];
 }
