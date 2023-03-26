b321e2a567b56f100dfba22f5e9a2552d9497a8b
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-26 10:56:14 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,4 +1,7 @@
 function solution(array) {
-    let big = [...array].sort((a,b)=>b-a)[0];
+    // 첫 번째 풀이 => arr.sort
+    // let big = [...array].sort((a,b)=>b-a)[0];
+    // 두 번째 풀이 => Math.max 쓰기
+    let big = Math.max(...array)
     return [big,array.indexOf(big)];
 }
