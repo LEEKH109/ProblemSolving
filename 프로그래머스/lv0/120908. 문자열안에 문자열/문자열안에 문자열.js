73da6664b651f3f14077bcf5dac11f753147f436
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-26 11:05:12 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,3 +1,6 @@
 function solution(str1, str2) {
-    return str1.indexOf(str2) === -1 ? 2 : 1;
+    // 첫 번째 풀이 => indexOf
+    // return str1.indexOf(str2) === -1 ? 2 : 1;
+    // 두 번째 풀이 => includes
+    return str1.includes(str2) ? 1 : 2
 }
