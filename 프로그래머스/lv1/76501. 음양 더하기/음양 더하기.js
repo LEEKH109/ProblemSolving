f9b78bb0f90bd0dd9ede9fdc1f57da2d6163d8c5
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-28 21:44:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,9 @@
-function solution(absolutes, signs) { // 내장 메소드를 쓸 이유가 없어보인다.
-     let result=0;
-     for(let i=0;i<absolutes.length;i++){
-         signs[i] ? result+=absolutes[i] : result-=absolutes[i]
-        }
+function solution(absolutes, signs) { // 첫 번째 풀이 => 내장 메소드를 쓸 이유가 없어보인다.
+     //let result=0;
+     //for(let i=0;i<absolutes.length;i++){
+         //signs[i] ? result+=absolutes[i] : result-=absolutes[i]
+        //}
+    // 두 번째 풀이 => 내장 메소드를 사용했지만 for문보다 가독성이 떨어진다는 생각이 든다.
+    let result = absolutes.reduce((acc, val, i) => acc + val * (signs[i] ? 1 : -1), 0);
     return result;
 }
