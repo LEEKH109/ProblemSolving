0b9e25b9d32c5c171e49631140e5d94dd3864131
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-28 21:53:04 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,7 @@
 function solution(numbers) {
-    // 완전한 베열에서 필터링 하는 방식으로
-    let fullArr =[0,1,2,3,4,5,6,7,8,9];
-    return fullArr.filter(el => !numbers.includes(el)).reduce((a,b)=>a+b,0);
+    // 내 풀이 =>완전한 베열에서 필터링 하는 방식으로
+   // let fullArr =[0,1,2,3,4,5,6,7,8,9];
+   // return fullArr.filter(el => !numbers.includes(el)).reduce((a,b)=>a+b,0);
+    // 다른 분 풀이 => 전체합에서 바로 계산
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
