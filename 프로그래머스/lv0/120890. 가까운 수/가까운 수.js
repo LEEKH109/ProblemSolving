518400c3ebfff75b2af5b5d7c6c6c08610ee2d11
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-30 12:29:12 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,11 @@
+function solution(array, n) {
+    // 뇌 빼고 만든 풀이
+    //let newArr = [...array,n].sort((a,b)=>a-b);
+    //if(newArr.indexOf(n) === 0) return newArr[newArr.indexOf(n)+1];
+    //f(newArr.indexOf(n) === newArr.length-1) return newArr[newArr.indexOf(n)-1];
+    //return (Math.abs(newArr[newArr.indexOf(n)-1]-n) > Math.abs(newArr[newArr.indexOf(n)+1]-n) 
+            //? newArr[newArr.indexOf(n)+1] : newArr[newArr.indexOf(n)-1])
+    array.sort((a,b)=>a-b)
+    array.sort((a,b)=>Math.abs(n-a)<Math.abs(n-b) ? -1 : 1)
+    return array[0]
+}
