474e48c3f01e857f8c08f37900ba7d29850fa6cf
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 02:12:08 +0900

    [level 0] Title: 약수 구하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,12 @@
 function solution(n) {
-    let answer = [];
-    for(let i = 1; i <= Math.sqrt(n); i++){
-        if(n%i === 0){
-            i === Math.sqrt(n) ? answer.push(i) : answer.push(i,n/i);
-        }
-    }
-    return answer.sort((a,b) => a-b);
+    // 나의 풀이
+    // let answer = [];
+    // for(let i = 1; i <= Math.sqrt(n); i++){
+    //     if(n%i === 0){
+    //         i === Math.sqrt(n) ? answer.push(i) : answer.push(i,n/i);
+    //     }
+    // }
+    // return answer.sort((a,b) => a-b);
+    // 다른 사람 풀이
+    return Array(n).fill(0).map((v, index) => v+index+1).filter((v) => n%v===0);
 }
