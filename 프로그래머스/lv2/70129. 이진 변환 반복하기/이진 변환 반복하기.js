8e2dbe39b49c74146ccf9bfb7ea17317fe23b4c4
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-30 14:28:29 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.94 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,15 +1,28 @@
 function solution(s) {
-    let countWork = 0;
-    let countZero = 0;
-    let string = s;
+    // 처음 풀이
+    // let countWork = 0;
+    // let countZero = 0;
+    // let string = s;
+    // function work(string){
+    //     let noZero = string.split('').filter(el => el !== '0').reduce((a,b)=>a+b)
+    //     countWork += 1
+    //     countZero += (string.length-noZero.length)
+    //     return noZero.length.toString(2)
+    // }
+    // while(string !== '1'){
+    //     string = work(string)
+    // }
+    // return [countWork, countZero]
+    // 개선된 풀이
+    let result =[0,0];
     function work(string){
         let noZero = string.split('').filter(el => el !== '0').reduce((a,b)=>a+b)
-        countWork += 1
-        countZero += (string.length-noZero.length)
+        result[0] += 1
+        result[1] += (string.length-noZero.length)
         return noZero.length.toString(2)
     }
-    while(string !== '1'){
-        string = work(string)
+    while(s !== '1'){
+        s = work(s)
     }
-    return [countWork, countZero]
+    return result
 }
