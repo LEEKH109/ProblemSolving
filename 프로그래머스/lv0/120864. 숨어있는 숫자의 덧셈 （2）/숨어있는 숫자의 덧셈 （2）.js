f31b8f8429c5b3200670d4d9ceb155fd1af2e38c
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 22:50:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,16 +1,19 @@
 function solution(my_string) {
-    let stack = '';
-    let numArr = [];
+//     first try => success
+//     let stack = '';
+//     let numArr = [];
     
-    for(const el of my_string){
-        if(el.match(/[0-9]/g)){
-            stack+=el
-        }else{
-            stack === '' ? '': numArr.push(stack) 
-            stack =''
-        }
-    };
-    if(stack !== '') numArr.push(stack);
+//     for(const el of my_string){
+//         if(el.match(/[0-9]/g)){
+//             stack+=el
+//         }else{
+//             stack === '' ? '': numArr.push(stack) 
+//             stack =''
+//         }
+//     };
+//     if(stack !== '') numArr.push(stack);
     
-    return numArr.reduce((acc,index)=>acc+Number(index),0);
+//     return numArr.reduce((acc,index)=>acc+Number(index),0);
+//     second try
+    return my_string.replace(/[A-z]/g, " ").split(" ").reduce((acc,index) => acc+Number(index),0)
 }
