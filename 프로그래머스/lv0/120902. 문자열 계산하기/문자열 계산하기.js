e4db2ebbde46caa076864d0ae6ea73f958d72975
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 11:31:58 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,12 +1,27 @@
 function solution(my_string) {
-  let arr = my_string.split(' ');
-  let result = parseInt(arr[0]);
+//     내 풀이
+//     let arr = my_string.split(' ');
+//     let result = parseInt(arr[0]);
   
-  for (let i = 1; i < arr.length; i += 2) {
-    let operator = arr[i];
-    let rightNum = parseInt(arr[i + 1]);
-    operator === '+' ? result += rightNum : result -= rightNum
-  }
-  
-  return result;
+//     for (let i = 1; i < arr.length; i += 2) {
+//         let operator = arr[i];
+//         let rightNum = parseInt(arr[i + 1]);
+//         operator === '+' ? result += rightNum : result -= rightNum
+//     }
+//     return result;
+//     스택을 이용한 풀이
+    const stack = [];
+
+    let sign = 1;
+    for (const ch of my_string.split(" ")) {
+        if (ch === "+") {
+            sign = 1;
+        } else if (ch === "-") {
+            sign = -1;
+        } else {
+            stack.push(ch * sign);
+        }
+    }
+
+    return stack.reduce((a,b) => a + b, 0);
 }
