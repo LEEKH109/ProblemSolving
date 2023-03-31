5f280609c0511395c2298a8231792de8abcfed44
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 22:14:56 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -18,8 +18,11 @@ function solution(numbers) {
     //     }
     // }
     // return start
-    // third try
+    // third try => success
+    // numbers.sort((a,b) => a-b)
+    // return numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2] 
+    //     ? numbers[0]*numbers[1] : numbers[numbers.length-1]*numbers[numbers.length-2]
+    // fourth try
     numbers.sort((a,b) => a-b)
-    return numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2] ? numbers[0]*numbers[1] : numbers[numbers.length-1]*numbers[numbers.length-2]
-    
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2])
 }
