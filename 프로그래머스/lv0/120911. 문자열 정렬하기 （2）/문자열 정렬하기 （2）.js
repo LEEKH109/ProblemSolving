884c856422e402a98e4efdb95d89848db19dbc34
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 12:59:51 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,6 @@
 function solution(my_string) {
-    return my_string.split('').map(el => el.toLowerCase()).sort().reduce((acc,el)=>acc+el,'')
+    // 첫 번째 풀이
+    // return my_string.split('').map(el => el.toLowerCase()).sort().reduce((acc,el)=>acc+el,'')
+    // 두 번째 풀이
+    return [...my_string.toLowerCase()].sort().join('')
 }
