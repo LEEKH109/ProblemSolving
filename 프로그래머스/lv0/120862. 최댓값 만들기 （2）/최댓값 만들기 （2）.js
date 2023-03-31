7424809e24076045967ad8762d55ca82e02c013d
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-03-31 22:12:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-    // first try
+    // first try => fail
     // let plusArr = numbers.filter(el=> el !== 0).sort((a,b)=>b-a);
     // if(plusArr.length === 1){
     //     return plusArr[0];
@@ -10,13 +10,16 @@ function solution(numbers) {
     // let plusMax = plusArr[0]*plusArr[1];
     // let minusMax = minusArr[0]*minusArr[1];
     // return plusMax > minusMax ? plusMax : minusMax;
-    // second try
-    let start = numbers[0]*numbers[1];
-    for(i=0;i<numbers.length;i++){
-        for(j=i+1;j<numbers.length;j++){
-            if(numbers[i]*numbers[j] > start) start = numbers[i]*numbers[j]
-        }
-    }
-    return start
+    // second try => success
+    // let start = numbers[0]*numbers[1];
+    // for(i=0;i<numbers.length;i++){
+    //     for(j=i+1;j<numbers.length;j++){
+    //         if(numbers[i]*numbers[j] > start) start = numbers[i]*numbers[j]
+    //     }
+    // }
+    // return start
+    // third try
+    numbers.sort((a,b) => a-b)
+    return numbers[0]*numbers[1] > numbers[numbers.length-1]*numbers[numbers.length-2] ? numbers[0]*numbers[1] : numbers[numbers.length-1]*numbers[numbers.length-2]
     
 }
