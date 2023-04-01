c838a0f01133b8b0efbf2d38d396512cf7ff7d57
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-01 20:41:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,14 +1,7 @@
 function solution(spell, dic) {
-    // 첫 번째 풀이
-    // let filteredDic = [...dic];
-    // for(const el of spell){
-    //     filteredDic = filteredDic.filter((idx) => (idx.includes(el)))
-    // }
-    // return filteredDic.length !== 0 ? 1 : 2;
-    // 두 번째 풀이 => spell 이 중복되서 들어간 경우도 고려
     let filteredDic = [...dic];
     for(const el of spell){
-        filteredDic = filteredDic.filter((idx) => (idx.includes(el)&&(idx.indexOf(el) === idx.lastIndexOf(el))))
+        filteredDic = filteredDic.filter(idx => idx.includes(el))
     }
     return filteredDic.length !== 0 ? 1 : 2;
 }
