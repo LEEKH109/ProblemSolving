796f40991c1ce9e0204b7991c17a33d1172d45df
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-01 15:51:19 +0900

    [level 0] Title: 안전지대, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,28 +1,40 @@
 function solution(board) {
-    let result = Array.from({length: board.length}, () => Array.from({length: board.length}, () => 0));
-    const n = board.length;
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n; j++){
-            if(board[i][j] === 1){
-                result[i][j] = 1;
-                if(i-1 >= 0 && j-1 >= 0) result[i-1][j-1] = 1;
-                if(i-1 >= 0) result[i-1][j] = 1;
-                if(i-1 >= 0 && j+1 < n) result[i-1][j+1] = 1;
-                if(j-1 >= 0) result[i][j-1] = 1;
-                if(j+1 < n) result[i][j+1] = 1;
-                if(i+1 < n && j-1 >= 0) result[i+1][j-1] = 1;
-                if(i+1 < n) result[i+1][j] = 1;
-                if(i+1 < n && j+1 < n) result[i+1][j+1] = 1;
-            }
-        }
-    }
-    let count = 0;
-    for (let a = 0; a < n; a++) {
-        for (let b = 0; b < n; b++) {
-            if (result[a][b] === 0) {
-                count++;
-            }
-        }
-    }
-    return count;
+    // 첫 번째 풀이
+    // let result = Array.from({length: board.length}, () => Array.from({length: board.length}, () => 0));
+    // const n = board.length;
+    // for(let i=0; i<n; i++){
+    //     for(let j=0; j<n; j++){
+    //         if(board[i][j] === 1){
+    //             result[i][j] = 1;
+    //             if(i-1 >= 0 && j-1 >= 0) result[i-1][j-1] = 1;
+    //             if(i-1 >= 0) result[i-1][j] = 1;
+    //             if(i-1 >= 0 && j+1 < n) result[i-1][j+1] = 1;
+    //             if(j-1 >= 0) result[i][j-1] = 1;
+    //             if(j+1 < n) result[i][j+1] = 1;
+    //             if(i+1 < n && j-1 >= 0) result[i+1][j-1] = 1;
+    //             if(i+1 < n) result[i+1][j] = 1;
+    //             if(i+1 < n && j+1 < n) result[i+1][j+1] = 1;
+    //         }
+    //     }
+    // }
+    // let count = 0;
+    // for (let a = 0; a < n; a++) {
+    //     for (let b = 0; b < n; b++) {
+    //         if (result[a][b] === 0) {
+    //             count++;
+    //         }
+    //     }
+    // }
+    // return count;
+    // 다른 분 풀이
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let safezone = 0;
+
+    board.forEach((row, y, self) => row.forEach((it, x) => {
+        if (it === 1) return false;
+        return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+               ? false : safezone++;
+    }));
+
+    return safezone;
 }
