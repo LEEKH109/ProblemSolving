234dccd45b8d200327ebc8ec0878339421b9a89d
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-02 01:45:47 +0900

    [level 0] Title: 특이한 정렬, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,3 +1,6 @@
 function solution(numlist, n) {
-    return numlist.sort((a,b)=> b-a ).sort((a,b)=> Math.abs(n-a) >= Math.abs(n-b) ? 1: -1);
+    // first try => success
+    // return numlist.sort((a,b)=> b-a ).sort((a,b)=> Math.abs(n-a) >= Math.abs(n-b) ? 1: -1);
+    // second try
+     return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
