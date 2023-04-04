119db8ea0dbdadbea0882fb0dd311b8863ea1ff6
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-04 11:52:33 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,24 +1,36 @@
 function solution(a, b) {
-    // for 반복문을 이용해서 최대공약수 구하기 => 시간복잡도 O(n)
-    let divG = 1; // 공간복잡도 O(1)
-    for(let i =1; i<=a; i++){
-        if(a%i === 0 && b%i === 0){divG = i}
-        i++
-    }
-    b /= divG;
+//     나의 풀이
+//     // for 반복문을 이용해서 최대공약수 구하기 => 시간복잡도 O(n)
+//     let divG = 1; // 공간복잡도 O(1)
+//     for(let i =1; i<=a; i++){
+//         if(a%i === 0 && b%i === 0){divG = i}
+//         i++
+//     }
+//     b /= divG;
     
-    let factors = [];
-    let divisor = 2;
-    // while 반복문으로 소인수 분해 => 0(1) < 시간복잡도 < O(n)
-    while (b >= 2) {
-     if (b % divisor == 0) {
-       factors.push(divisor);
-       b /= divisor;
-     } else {
-       divisor++;
-     }
+//     let factors = [];
+//     let divisor = 2;
+//     // while 반복문으로 소인수 분해 => 0(1) < 시간복잡도 < O(n)
+//     while (b >= 2) {
+//      if (b % divisor == 0) {
+//        factors.push(divisor);
+//        b /= divisor;
+//      } else {
+//        divisor++;
+//      }
+//     }
+//     return factors.reduce((acc, el) => {
+//         return el > 5 || el === 3 ? acc+1 : acc ;
+//         }, 0) === 0 ? 1 : 2;
+//     다른분 풀이
+    let n = 1;
+    for (let i = 1; i <= Math.min(a,b); i++) {
+        if (a%i===0 && b%i===0) n = i;
     }
-    return factors.reduce((acc, el) => {
-        return el > 5 || el === 3 ? acc+1 : acc ;
-        }, 0) === 0 ? 1 : 2;
+
+    b/=n;
+    while (b%2===0) b/=2;
+    while (b%5===0) b/=5;
+
+    return b === 1 ? 1 : 2;   
 }
