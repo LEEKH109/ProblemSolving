ad3ca8eecaf4d9b1ffbeea3a3f2dcc536d1c0de6
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-08 20:11:13 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,5 +1,10 @@
 function solution(n) {
-    let strNum = Math.sqrt(n).toString().split('.')
-    return strNum.length === 1 ? Math.pow(Math.sqrt(n)+1,2) : -1
+    // 첫 번째 풀이
+    // let strNum = Math.sqrt(n).toString().split('.');
+    // return strNum.length === 1 ? Math.pow(Math.sqrt(n)+1,2) : -1;
+    // 두 번째 풀이
+    let root = Math.sqrt(n);
+    let result = (root%1 === 0 ? (root+1)*(root+1) : -1);
+    return result;
     
 }
