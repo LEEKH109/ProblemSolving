cacc3754776aaa302ffbe3c56e31e19a79f8743c
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-09 23:28:17 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -7,5 +7,7 @@ function solution(str) {
     // 두 번째 풀이=> 리팩토링
     // return '*'.repeat(str.length-4)+str.slice(-4);
     // 세번째 풀이 Array.fill이용
-    return [...str].fill("*",0,str.length-4).join("")
+    // return [...str].fill("*",0,str.length-4).join("")
+    // 다른 분 풀이 => 정규표현식
+    return str.replace(/\d(?=\d{4})/g, "*");
 }
