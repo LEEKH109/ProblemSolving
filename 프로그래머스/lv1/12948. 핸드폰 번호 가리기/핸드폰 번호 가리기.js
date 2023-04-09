1059d78d9bf8d42bff9fbff571b0610cc77f69ab
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-09 23:26:12 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,10 @@
-function solution(phone_number) {
-    let numLength = phone_number.length;
-    let open = phone_number.slice(numLength-4,numLength)
-    let secret = '*'.repeat(numLength-4)
-    return secret+open;
+function solution(str) {
+    // 첫 번째 풀이
+    // let numLength = phone_number.length;
+    // let open = phone_number.slice(numLength-4,numLength)
+    // let secret = '*'.repeat(numLength-4)
+    // return secret+open;
+    // 두 번째 풀이=> 리팩토링
+    return '*'.repeat(str.length-4)+str.slice(-4);
+    
 }
