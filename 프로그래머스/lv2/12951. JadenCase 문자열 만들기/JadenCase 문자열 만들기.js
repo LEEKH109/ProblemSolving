843d58d9ad5610af4340c79d9856b29761e58a10
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-09 21:15:25 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,7 @@
 function solution(s) {
-    
-    let answer = s.toLowerCase().split(' ').map(el=> el? el[0].toUpperCase()+el.slice(1) : '').join(' ')
-    return answer;
+    // 나의 풀이
+    // let answer = s.toLowerCase().split(' ').map(el=> el? el[0].toUpperCase()+el.slice(1) : '').join(' ')
+    // return answer;
+    // 다른 분의 풀이 => charAt을 사용해서 중복 공백 처리
+    return s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
 }
