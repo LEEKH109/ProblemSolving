3c02f7bf0e4f17379eef7601f6473ce3c39143dc
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-11 22:10:25 +0900

    [unrated] Title: 덧칠하기, Time: 5.37 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,17 +1,17 @@
-// 나의 풀이 => 11번 17번이 시간초과로 안풀린다.
-// function solution(n, m, section) {
-//     let wall = Array(n).fill(true);
-//     let count = 0;
-//     for(el of section){
-//         wall[el-1] = false;
-//     }
-//     while(wall.includes(false)){
-//         let start = wall.indexOf(false);
-//         wall = [...wall.slice(0,start), ...wall.slice(start,start+m).map(el => el = true), ...wall.slice(start+m)];
-//         count++;
-//     }
-//     return count;
-// }
+
+function solution(n, m, section) {
+    let wall = Array(n).fill(true);
+    let count = 0;
+    for(el of section){
+        wall[el-1] = false;
+    }
+    while(wall.includes(false)){
+        let start = wall.indexOf(false);
+        wall = [...wall.slice(0,start), ...wall.slice(start,start+m).map(el => el = true), ...wall.slice(start+m)];
+        count++;
+    }
+    return count;
+}
 // 시간 초과 안나는 다른 분 풀이
 // function solution(n, m, section) {
 //   let answer = 0;
