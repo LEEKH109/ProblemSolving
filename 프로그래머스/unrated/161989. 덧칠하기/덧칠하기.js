88f885f09319369e9d47b02cd4e06600e109d32f
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-11 22:09:58 +0900

    [unrated] Title: 덧칠하기, Time: 6.73 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -13,15 +13,36 @@
 //     return count;
 // }
 // 시간 초과 안나는 다른 분 풀이
+// function solution(n, m, section) {
+//   let answer = 0;
+//   let max = 0;
+//   section.forEach((s) => {
+//     if (s > max) {
+//       answer++;
+//       max = s + m - 1;
+//     }
+//   });
+//   return answer;
+// }
+// 내 풀이와 비슷한 풀이
 function solution(n, m, section) {
-  let answer = 0;
-  let max = 0;
-  section.forEach((s) => {
-    if (s > max) {
-      answer++;
-      max = s + m - 1;
+    let map = Array(n).fill(1)
+    section.forEach(el=>{
+        map[el-1] = 0
+    })
+
+    var answer = 0;
+
+    for(let i=0;i<map.length;i++){
+        if(map[i] === 0){
+            answer++
+            for(let j=i;j<i+m;j++){
+                map[j] = 1 
+            }
+            i += m-1
+        }
     }
-  });
-  return answer;
+
+    return answer;
 }
 
