6fbec459e1bdfe6bcb72fc853ac1dd50b7745926
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-04-15 10:40:52 +0900

    [level 2] Title: 가장 큰 수, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,27 @@
 function solution(numbers) {
-    numbers = numbers.map( el => el.toString() );
-    numbers.sort((a, b) => Number(b + a) - Number(a + b));
-    return numbers[0] !== '0' ? numbers.join('') : '0';
+    // 첫 번째 풀이
+    // numbers = numbers.map( el => el.toString() );
+    // numbers.sort((a, b) => Number(b + a) - Number(a + b));
+    // return numbers[0] !== '0' ? numbers.join('') : '0';
+    // 다른 분 풀이
+    const buckets = [];
+
+    for (const n of numbers){
+        const s = String(n), l = s.length;
+
+        let j = '';
+        for (let i = 0; i < 4; i++){
+            j += s[l > i ? i : i % l];
+        }
+        j = 9999 - j;
+
+        buckets[j] = buckets[j] ? buckets[j] + s : s;
+    }
+
+    const ans = buckets.join``;
+
+    return ans[0] === '0' ? '0' : ans;
+
 }
 
 	
