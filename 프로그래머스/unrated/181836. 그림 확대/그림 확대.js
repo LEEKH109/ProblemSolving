fd824278df3fe81da3a2e02197cab066ec935033
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-05-23 16:39:09 +0900

    [unrated] Title: 그림 확대, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -1,14 +1,20 @@
 function solution(picture, k) {
+    // let result = [];
+    // for(const el of picture){
+    //     let mulLetter = '';
+    //     for(const le of el){
+    //         mulLetter += le.repeat(k)
+    //     };
+    //     for(i=0;i<k;i++){
+    //         result.push(mulLetter)
+    //     };
+    // }
+    // return result;
+    // //시간복잡도 O(n^2)
     let result = [];
-    for(const el of picture){
-        let mulLetter = '';
-        for(const le of el){
-            mulLetter += le.repeat(k)
-        };
-        for(i=0;i<k;i++){
-            result.push(mulLetter)
-        };
-    }
+    picture.map((v) => {
+        const cur = [...v].map((c) => c.repeat(k)).join('');
+        for(let i = 0; i < k; i++) result.push(cur);
+    });
     return result;
-    //시간복잡도 O(n^2)
 }
