5e397fdfcdbbf87e69279825f6793f3ec69f6005
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-12 14:41:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 456 ms, Memory: 31260 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lanCnt = sc.nextInt();
+		int K = sc.nextInt();
+		long[] tmpArr = new long[lanCnt];
+		long maxCut = 0;
+
+		for (int tmp = 0; tmp < lanCnt; tmp++) {
+			tmpArr[tmp] = sc.nextInt();
+			maxCut = Math.max(maxCut, tmpArr[tmp]);
+		}
+
+		long minCut = 1; // 랜선의 최소 길이
+		long resultCut = 0;
+
+		while (minCut <= maxCut) {
+			long midCut = (minCut + maxCut) / 2;
+			long lanSum = 0;
+
+			for (int tmp = 0; tmp < lanCnt; tmp++) {
+				lanSum += (tmpArr[tmp] / midCut);
+			}
+
+			if (lanSum >= K) {
+				resultCut = midCut;
+				minCut = midCut + 1;
+			} else {
+				maxCut = midCut - 1;
+			}
+		}
+		System.out.println(resultCut);
+	}
+}
