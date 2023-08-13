28839ffb6dfe764c4e2633d4ba33a516b3f3beab
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-14 01:01:31 +0900

    [D3] Title: 오목 판정, Time: 173 ms, Memory: 29,272 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,47 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for (int test_case = 1; test_case <= tc; test_case++) {
-            int arr_size = Integer.parseInt(br.readLine());
-            char[][] omokBoard = new char[arr_size][];
-            for (int x = 0; x < arr_size; x++) {
-                omokBoard[x] = br.readLine().toCharArray();
-            }
-            int[] dx = {1, 1, 1, 0};
-            int[] dy = {1, 0, -1, 1};
-            boolean result = false;
-            find: for (int y = 0; y < arr_size; y++) {
-                for (int x = 0; x < arr_size; x++) {
-                    if (omokBoard[x][y] != 'o') continue;
-                    for (int dir = 0; dir < 4; dir++) {
-                        int cnt = 0;
-                        for (int k = -2; k <= 2; k++) {
-                            int nx = x + k * dx[dir], ny = y + k * dy[dir];
-                            if (nx < 0 || nx >= arr_size || ny < 0 || ny >= arr_size) {
-                                break;
-                            }
-                            if (omokBoard[nx][ny] == '.') {
-                                break;
-                            }
-                            cnt++;
-                        }
-                        if (cnt == 5) {
-                            result = true;
-                            break find;
+    static class Qnum {
+        int x, y, direction, counter;
+        Qnum(int x, int y, int direction, int counter) {
+            this.x = x;
+            this.y = y;
+            this.direction = direction;
+            this.counter = counter;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int test_case = scanner.nextInt();
+        int[] dx = {0, 1, 1, 1}, dy = {1, 1, 0, -1}; // 오른쪽, 오른쪽 대각선 아래, 아래, 왼쪽 대각선 아래 방향만 확인
+
+        for (int t = 1; t <= test_case; t++) {
+            int n, found = 0;
+            char[][] board = new char[25][25];
+            n = scanner.nextInt();
+            Queue<Qnum> q = new LinkedList<>();
+            for (int i = 0; i < n; i++) {
+                String row = scanner.next();
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = row.charAt(j);
+                    if (board[i][j] == 'o') {
+                        for (int k = 0; k < 4; k++) { // 4방향만 확인
+                            q.add(new Qnum(i, j, k, 1));
                         }
                     }
                 }
             }
-            sb.append("#").append(test_case).append(" ").append(result ? "YES" : "NO").append("\n");
+
+            while (!q.isEmpty()) {
+                Qnum current = q.poll();
+
+                if (current.counter >= 5) {
+                    found = 1;
+                    break;
+                }
+                int newx = current.x + dx[current.direction];
+                int newy = current.y + dy[current.direction];
+                if (newx >= 0 && newx < n && newy >= 0 && newy < n && board[newx][newy] == 'o') {
+                    q.add(new Qnum(newx, newy, current.direction, current.counter + 1));
+                }
+            }
+            System.out.print("#" + t + " ");
+            if (found == 0) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
         }
-        System.out.print(sb.toString());
-        br.close();
+        scanner.close();
     }
 }
