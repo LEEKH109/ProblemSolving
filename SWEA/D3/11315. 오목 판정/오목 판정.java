0e1329b5339132f2375f89ebe06103ec51ff7f34
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-19 21:30:22 +0900

    [D3] Title: 오목 판정, Time: 178 ms, Memory: 28,256 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,58 +1,38 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
+
 public class Solution {
-    static class Qnum {
-        int x, y, direction, counter;
-        Qnum(int x, int y, int direction, int counter) {
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
-            this.counter = counter;
-        }
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int test_case = scanner.nextInt();
-        int[] dx = {0, 1, 1, 1}, dy = {1, 1, 0, -1}; // 오른쪽, 오른쪽 대각선 아래, 아래, 왼쪽 대각선 아래 방향만 확인
-
-        for (int t = 1; t <= test_case; t++) {
-            int n, found = 0;
-            char[][] board = new char[25][25];
-            n = scanner.nextInt();
-            Queue<Qnum> q = new LinkedList<>();
-            for (int i = 0; i < n; i++) {
-                String row = scanner.next();
-                for (int j = 0; j < n; j++) {
-                    board[i][j] = row.charAt(j);
-                    if (board[i][j] == 'o') {
-                        for (int k = 0; k < 4; k++) { // 4방향만 확인
-                            q.add(new Qnum(i, j, k, 1));
-                        }
-                    }
-                }
-            }
-
-            while (!q.isEmpty()) {
-                Qnum current = q.poll();
-
-                if (current.counter >= 5) {
-                    found = 1;
-                    break;
-                }
-                int newx = current.x + dx[current.direction];
-                int newy = current.y + dy[current.direction];
-                if (newx >= 0 && newx < n && newy >= 0 && newy < n && board[newx][newy] == 'o') {
-                    q.add(new Qnum(newx, newy, current.direction, current.counter + 1));
-                }
-            }
-            System.out.print("#" + t + " ");
-            if (found == 0) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
-            }
-        }
-        scanner.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <=T; tc++) {
+			int N = sc.nextInt();//배열의 길이
+			char [][] tmpArr = new char[N][];
+			for(int i = 0; i < N; i++) {
+				tmpArr[i] = sc.next().toCharArray();
+			}//문자 입력 끝 
+			int [] dx = {1,0,1,-1};//델타 사용
+			int [] dy = {0,1,1,1};//총 4방향만 점검하면 됩니다.
+			boolean result = false;
+			Loop: for(int y = 0; y < N; y++) {
+				for(int x = 0; x < N; x++) {//배열을 완전 탐색 할 겁니다.
+					for(int dir = 0; dir < 4; dir++) { //4방위를 돌리기 위한 반복문입니다.
+						for(int omok = 0; omok <5; omok++) {
+							int xd = x+dx[dir]*omok;
+							int yd = y+dy[dir]*omok;
+							if(xd < 0 || xd >= N || yd < 0 || yd >= N) {
+								break; // 시작좌표가 배열의 범위 밖으로 벗어난 경우 진행하지 않습니다.
+							}else if(tmpArr[xd][yd] != 'o') {
+								break;
+							}else if(tmpArr[xd][yd] == 'o' && omok == 4) {
+								result = true;
+								break Loop;// 더 찾을 필요가 없다.
+							}
+						}
+					}
+				}
+			}
+			System.out.println(String.format("#%d %s", tc, (result ? "YES" : "NO")));	
+		}
+		sc.close();
+	}
 }
