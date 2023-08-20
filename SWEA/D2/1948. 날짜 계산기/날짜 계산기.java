373b4b73488adf631aa3c49dd1604f15230dffc6
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-20 12:00:06 +0900

    [D2] Title: 날짜 계산기, Time: 130 ms, Memory: 20,552 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -1,49 +1,69 @@
 import java.util.Scanner;
 
-public class Solution {	
+public class Solution {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);// 입력을 위한 스캐너 선언
+		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt(); // 전체 테스트 케이스 수 입력
 		for(int tc = 1; tc <= T; tc++) {
 			int startM = sc.nextInt();
 			int startD = sc.nextInt();
 			int endM = sc.nextInt();
 			int endD = sc.nextInt();
-			int result = dayConvert(endM, endD)-dayConvert(startM, startD)+1;
+			int [] tmpArr = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			for(int i = 0; i< 12; i++) {
+				tmpArr[i+1] += tmpArr[i];
+			}
+			int result = (tmpArr[endM-1]+endD) - (tmpArr[startM-1]+startD)+1;
 			System.out.println(String.format("#%d %d", tc, result));
 		}
 		sc.close();
 	}
-	public static int dayConvert(int month, int day) {
-		int returnDay = 0;
-		switch (month-1) {
-		case 12:
-			returnDay+=31;
-		case 11:
-			returnDay+=30;
-		case 10:
-			returnDay+=31;
-		case 9:
-			returnDay+=30;
-		case 8:
-			returnDay+=31;
-		case 7:
-			returnDay+=31;
-		case 6:
-			returnDay+=30;
-		case 5:
-			returnDay+=31;
-		case 4:
-			returnDay+=30;
-		case 3:
-			returnDay+=31;
-		case 2:
-			returnDay+=28;
-		case 1:
-			returnDay+=31;
-		default:
-			break;
-		}
-		return returnDay+day;
-	}
 }
+//기존 방법
+//public class 날짜계산기switch {	
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);// 입력을 위한 스캐너 선언
+//		int T = sc.nextInt(); // 전체 테스트 케이스 수 입력
+//		for(int tc = 1; tc <= T; tc++) {
+//			int startM = sc.nextInt();
+//			int startD = sc.nextInt();
+//			int endM = sc.nextInt();
+//			int endD = sc.nextInt();
+//			int result = dayConvert(endM, endD)-dayConvert(startM, startD)+1;
+//			System.out.println(String.format("#%d %d", tc, result));
+//		}
+//		sc.close();
+//	}
+//	public static int dayConvert(int month, int day) {
+//		int returnDay = 0;
+//		switch (month-1) {
+//		case 12:
+//			returnDay+=31;
+//		case 11:
+//			returnDay+=30;
+//		case 10:
+//			returnDay+=31;
+//		case 9:
+//			returnDay+=30;
+//		case 8:
+//			returnDay+=31;
+//		case 7:
+//			returnDay+=31;
+//		case 6:
+//			returnDay+=30;
+//		case 5:
+//			returnDay+=31;
+//		case 4:
+//			returnDay+=30;
+//		case 3:
+//			returnDay+=31;
+//		case 2:
+//			returnDay+=28;
+//		case 1:
+//			returnDay+=31;
+//		default:
+//			break;
+//		}
+//		return returnDay+day;
+//	}
+//}
