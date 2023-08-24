3c6f5c9f6c533864e37c8b68b4b12e70de6cf96a
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-24 17:21:25 +0900

    [D2] Title: 스도쿠 검증, Time: 135 ms, Memory: 23,648 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,46 +1,60 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+//누적합 버전
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		int N = 9;
-		for(int tc = 1; tc <=T; tc++) {
+		for (int tc = 1; tc <= T; tc++) {
+			int sdokucheck = 1;
 			int [][] sdoku = new int [N][N];
-			for(int y = 0; y < N; y++) {
-				for(int x = 0; x < N; x++) {
+			for(int x = 0; x < N; x++) {
+				for(int y = 0; y < N; y++) {
 					sdoku[x][y] = sc.nextInt();
+					if(x > 0) {
+						sdoku[x][y]+=sdoku[x-1][y];
+					}
+					
 				}
-			}//스도큐 배열 입력 완료
-		//가로 스도쿠 탐색
-		int sdokucheck = 1;
-		fail: for(int i = 0; i < N; i++) {
-			int sumx = 0;
-			int sumy = 0;
-			for(int j = 0; j < N; j++) {
-				sumx+=sdoku[i][j];
-				sumy+=sdoku[j][i];
-			}
-			if(sumx != 45 || sumy != 45) {
-				sdokucheck = 0;
-				break fail;
-			}
-		}// 가로 세로 탐색 종료
-		fail: for(int y = 0; y < N; y+=3) {
-			for(int x = 0; x < N; x+=3) {
-				int sum = 0;
-				for(int i = 0; i < 3; i++) {
-					for(int j = 0; j < 3; j++) {
-						sum+=sdoku[x+i][y+j];
+			}//스도큐 배열 세로 누적합 입력 완료
+//			for(int i = 0; i < N ; i ++) {
+//				System.out.println(Arrays.toString(sdoku[i]));
+//			}// 세로 누적합 출력
+			if(sdokucheck == 1) {
+				for(int i = 0; i < N; i++) {
+					if(sdoku[N-1][i]%45 != 0) {
+						sdokucheck = 0;
+						break;
 					}
 				}
-				if(sum != 45) {
-					sdokucheck = 0;
-					break fail;
+			}// 누적합이 45 아니면 스도쿠 아님
+			for(int x = 0; x < N; x++) {
+				for(int y = 1; y < N; y++) {
+						sdoku[x][y]+=sdoku[x][y-1];
 				}
-			}
-		}//스도큐 배열 입력 완료
-		System.out.println("#"+tc+" "+sdokucheck);	
+			}//스도큐 배열 가로 누적합 입력 완료
+//			for(int i = 0; i < N ; i ++) {
+//				System.out.println(Arrays.toString(sdoku[i]));
+//			}// 가로 누적합 출력
+			if(sdokucheck == 1) {
+				for(int i = 0; i < N; i++) {
+					if(sdoku[i][N-1]%45 != 0) {
+						sdokucheck = 0;
+						break;
+					}
+				}
+			}//45로 안 나눠 떨어지면 스도쿠 아님
+			//이제 9칸씩 의 스도쿠를 판별 해야 합니다. 총 9번입니다.
+			fail : for(int x = 2; x < N; x+=3) {
+				for(int y = 2; y < N; y+=3) {
+					if(sdoku[x][y]%45 != 0) {
+						sdokucheck = 0;
+						break fail;
+					}
+				}
+			}//스도큐 배열 가로 누적합 입력 완료
+			System.out.println("#" + tc + " " + sdokucheck);
 		}
 		sc.close();
 	}
