ad1e1c77be6ec8e7779fd7ae7c419de350eb5360
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-24 16:40:41 +0900

    [D2] Title: 스도쿠 검증, Time: 135 ms, Memory: 23,648 KB -BaekjoonHub

---
+++ SWEA/D2/1974. 스도쿠 검증/README.md
@@ -0,0 +1,11 @@
+# [D2] 스도쿠 검증 - 1974 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq) 
+
+### 성능 요약
+
+메모리: 23,648 KB, 시간: 135 ms, 코드길이: 1,029 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int N = 9;
+		for(int tc = 1; tc <=T; tc++) {
+			int [][] sdoku = new int [N][N];
+			for(int y = 0; y < N; y++) {
+				for(int x = 0; x < N; x++) {
+					sdoku[x][y] = sc.nextInt();
+				}
+			}//스도큐 배열 입력 완료
+		//가로 스도쿠 탐색
+		int sdokucheck = 1;
+		fail: for(int i = 0; i < N; i++) {
+			int sumx = 0;
+			int sumy = 0;
+			for(int j = 0; j < N; j++) {
+				sumx+=sdoku[i][j];
+				sumy+=sdoku[j][i];
+			}
+			if(sumx != 45 || sumy != 45) {
+				sdokucheck = 0;
+				break fail;
+			}
+		}// 가로 세로 탐색 종료
+		fail: for(int y = 0; y < N; y+=3) {
+			for(int x = 0; x < N; x+=3) {
+				int sum = 0;
+				for(int i = 0; i < 3; i++) {
+					for(int j = 0; j < 3; j++) {
+						sum+=sdoku[x+i][y+j];
+					}
+				}
+				if(sum != 45) {
+					sdokucheck = 0;
+					break fail;
+				}
+			}
+		}//스도큐 배열 입력 완료
+		System.out.println("#"+tc+" "+sdokucheck);	
+		}
+		sc.close();
+	}
+}
