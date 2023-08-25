f9a265ba94777543595754ec6a2dd2c6bbf74795
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-08-25 11:32:25 +0900

    [D2] Title: 간단한 369게임, Time: 125 ms, Memory: 20,244 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -4,11 +4,11 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		String result = "";
+		String result = ""; //출력할 결과는 한번에 문자열로 
 		for(int i = 1; i <= N ; i++) {
-			if(clap(i) == 0) {
+			if(clap(i) == 0) { // 0이 반환되면 3 6 9 대상 숫자가 아니므로 문자열에 그냥 더해준다.
 				result+=(i+"");
-			}else {
+			}else {//여기서부터는 반환 횟수에 따라 "-"를 달아준다.
 				for(int c = 0; c < clap(i); c++) {
 					result+="-";
 				}
@@ -18,14 +18,14 @@ public class Solution {
 		System.out.println(result);
 		sc.close();
 	}
-	public static int clap(int num) {
+	public static int clap(int num) {// 해당 숫자가 3 6 9 대상인지 판단하는 함수
 		int clapCnt = 0;
-		char [] tmp = (num+"").toCharArray();
-		for(char el : tmp) {
+		char [] tmp = (num+"").toCharArray(); // 숫자를 다 쪼개서
+		for(char el : tmp) { 
 			if(el == '3' || el== '6' || el =='9') {
-				clapCnt++;
+				clapCnt++;// 각각의 숫자들이 3 6 9에 해당 될 때마다 +1
 			}
 		}
-		return clapCnt;
+		return clapCnt; // 해당 횟수를 반환
 	}
 }
