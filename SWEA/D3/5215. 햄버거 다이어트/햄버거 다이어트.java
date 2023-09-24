a865307d922a221182ed96420d0001d90f843bec
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-09-24 16:57:50 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,327 ms, Memory: 23,888 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,34 +1,47 @@
 import java.util.Scanner;
 
 public class Solution {
+	static boolean [] checked;
+	static int [][] food;
+	static int maxTaste;
+	static int N, L;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();// 테스트케이스 수 입력
-		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			int L = sc.nextInt();
-			int maxTaste = 0;
-			int[][] food = new int[N][2];
-			for (int i = 0; i < N; i++) {
-				food[i][0] = sc.nextInt();
-				food[i][1] = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			N = sc.nextInt();
+			L = sc.nextInt();
+			maxTaste = 0;
+			food= new int [N][2];
+			checked = new boolean [N];
+			for(int i = 0; i < N; i++) {
+				food[i][0] = sc.nextInt();// 맛에 대한 점수
+				food[i][1] = sc.nextInt();// 칼로리
 			}
-			for (int i = 1; i < (1 << N); i++) { // 2^N 만큼 경우의 수 쨰기
-				int sumTaste = 0;
-				int cal = 0;
-				for (int j = 0; j < N; j++) {
-					if ((i & (1 << j)) > 0) {
-						sumTaste+=food[j][0];
-						cal+=food[j][1];
-					}
-				}
-				if(cal<= L) {
-					maxTaste = Math.max(maxTaste, sumTaste);
-				}
-
-			}
+			powerset(0);
 			System.out.println("#"+tc+" "+maxTaste);
 		}
 		sc.close();
+	}
+	
+	public static void powerset(int now) {
+		if(now == N) {
+			int cal = 0;
+			int sumTaste = 0;
+			for(int i = 0; i < N; i++) {
+				if(checked[i]) {
+					sumTaste+=food[i][0];
+					cal+=food[i][1];
+				}
+			}
+			if(cal<= L) {
+				maxTaste = Math.max(maxTaste, sumTaste);
+			}
+			return;
+		}
+		checked[now] = false;
+		powerset(now+1);
+		checked[now] = true;
+		powerset(now+1);
 	}
 }
