3df53fbf240f08b0e860ed798ae3f09b258c4d81
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-10-10 10:56:06 +0900

    [Silver II] Title: N과 M (12), Time: 228 ms, Memory: 20324 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N,M;
+	static int arr[], tmp[];
+	static boolean visited[];
+	static StringBuilder sb;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int [N];
+		visited = new boolean[N];
+		tmp = new int [M];
+		for(int i = 0 ; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		dfs(0,0);
+		System.out.println(sb);
+		sc.close();
+	}
+	
+	public static void dfs(int now, int depth) {
+		if(depth == M) {
+			for(int el : tmp) {
+				sb.append(el+" ");
+			}sb.append("\n");
+			return;
+		}
+		
+//		for(int i = 0; i < N ; i++) {
+//			if(arr[i] > prev) {
+//				System.out.println(prev+" < "+arr[i]);
+//				prev = arr[i];
+//				tmp[depth] = arr[i];
+//				dfs(depth+1);
+//			}
+//		}
+		int prev = 0;
+		for(int i = now; i < N; i++) {
+			if(arr[i] != prev) {
+				prev = arr[i];
+				tmp[depth] = arr[i];
+				dfs(i,depth+1);
+			}
+		}
+	}
+}
