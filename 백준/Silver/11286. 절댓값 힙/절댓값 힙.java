293401167782032852fb0c638dec2a0ac26f8976
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2023-12-19 19:34:18 +0900

    [Silver I] Title: 절댓값 힙, Time: 1348 ms, Memory: 104112 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -9,11 +9,12 @@ public class Main {
 		PriorityQueue<Integer>pq = new PriorityQueue<>(new Comparator<Integer>() {
 			@Override
 			public int compare(Integer o1, Integer o2) {
-				if(Math.abs(o1) == Math.abs(o2)) {
-					return o1-o2;
-				}else {
-					return Math.abs(o1) - Math.abs(o2);
-				}
+				if(Math.abs(o1) == Math.abs(o2)) return o1-o2;
+					// Min Heap이라는 가정 
+					// 1. 양수가 반환 => o2가 더 작으므로 o2의 우선순위가 더 높다.
+					// 2. 0이 반환 =>  o1과 o2의 우선순위가 동일 
+					// 3. 음수가 반환 => o1가 더 작으므로 o1의 우선순위가 더 높다.
+				return Math.abs(o1) - Math.abs(o2);
 			}
 		});
 		for(int i = 0; i < N; i++) {
