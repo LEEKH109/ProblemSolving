a4e84dcf6cc65d6c6d13885483984cf355854a81
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-06-20 23:01:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.44 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[][] jobs) {
@@ -8,20 +7,24 @@ class Solution {
         
         int nowTime = 0; // 현재 시간
         boolean[] visited = new boolean[jobs.length]; // 방문 체크 배열
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); // 소요 시간이 짧은 순으로 정렬되는 우선순위 큐
         int totalWaitTime = 0; // 총 대기 시간
         int completedJobs = 0; // 완료된 작업 수
         
         while (completedJobs < jobs.length) {
-            // 현재 시간 이하에 요청된 작업들을 모두 우선순위 큐에 추가
+            int shortestJobIndex = -1;
+            int shortestJobTime = Integer.MAX_VALUE;
+            
+            // 현재 시간 이하에 요청된 작업 중 가장 짧은 작업을 찾음
             for (int i = 0; i < jobs.length; i++) {
                 if (!visited[i] && jobs[i][0] <= nowTime) {
-                    pq.add(jobs[i]);
-                    visited[i] = true; // 방문 처리
+                    if (jobs[i][1] < shortestJobTime) {
+                        shortestJobTime = jobs[i][1];
+                        shortestJobIndex = i;
+                    }
                 }
             }
             
-            if (pq.isEmpty()) {
+            if (shortestJobIndex == -1) {
                 // 처리할 작업이 없으면 현재 시간을 다음 요청 시간으로 이동
                 for (int i = 0; i < jobs.length; i++) {
                     if (!visited[i]) {
@@ -31,12 +34,13 @@ class Solution {
                 }
             } else {
                 // 가장 짧은 작업을 처리
-                int[] currentJob = pq.poll();
-                nowTime += currentJob[1];
-                totalWaitTime += nowTime - currentJob[0];
+                visited[shortestJobIndex] = true;
+                nowTime += jobs[shortestJobIndex][1];
+                totalWaitTime += nowTime - jobs[shortestJobIndex][0];
                 completedJobs++;
             }
         }
+        
         // 평균 대기 시간 계산 및 반환 (소수점 이하 버림)
         return totalWaitTime / jobs.length;
     }
