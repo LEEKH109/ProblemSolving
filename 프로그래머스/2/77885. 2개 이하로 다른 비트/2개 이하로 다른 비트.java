77f68d6903a77340ff7df65507346aec5219ce45
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-07-02 17:16:29 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 83.81 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -2,30 +2,31 @@
 // ~10 -> ~11 로 변환
 // ~01 -> ~10 로 변환 왜 11이 아닌가? 값이 제이 작아야 하므로
 // ~00 -> ~01 로 변환
-// ~11 -> 이게 문제인데 이 경우에는 11을 잘라내고 다시 시작한다.
-// 마지막 조건으로 인한 탈출 조건
-// ex) 11111111(2^8-1) 이 경우는 어떻게 할거냐? => 111111111(2^9-1)
+// ~11 -> 이게 문제인데 그냥 0인 부분을 찾아서 떠나면 된다.
+// 예외 조건 전부 111 인경우 
+// 이 경우는 1011 이렇게 맨앞만 01 -> 10으로 스왑 맨앞에 0이 있다고 생각하면 쉽다.
  class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
             long num = numbers[i];
-
             if (num % 2 == 0) {
+                // 10 -> 11
+                // 00 -> 01 인 경우
                 answer[i] = num + 1;
             } else {
-                String numTo2 = Long.toBinaryString(num);
-                if (numTo2.contains("0")) {
-                    int idx = numTo2.lastIndexOf("0");
-                    numTo2 = numTo2.substring(0, idx) + "10" + numTo2.substring(idx + 2);
+                String binary = Long.toBinaryString(num);
+                if (binary.contains("0")) {
+                    // 01 -> 10 인 경우
+                    int idx = binary.lastIndexOf("0");
+                    binary = binary.substring(0, idx) + "10" + binary.substring(idx + 2);
                 } else {
-                    numTo2 = "10" + numTo2.substring(1);
+                    binary = "10" + binary.substring(1);
                 }
-                answer[i] = Long.parseLong(numTo2, 2);
+                answer[i] = Long.parseLong(binary, 2);
             }
         }
-
         return answer;
     }
 }
