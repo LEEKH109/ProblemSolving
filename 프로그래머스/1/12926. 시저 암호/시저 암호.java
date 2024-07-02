8985175d95f9f25083bb6dad1534a7515e5df0fb
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-07-02 12:06:35 +0900

    [level 1] Title: 시저 암호, Time: 0.50 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,25 +1,39 @@
 class Solution {
     public String solution(String s, int n) {
-        // 아스키로 간단하게 생각?
-        // 대문자 65 ~ 90 소문자 97 ~ 122 아스키 범위 정하기
         char[] arr = s.toCharArray();
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == ' ') continue;
-            if(arr[i] < 91){
-                if(arr[i] + n > 90){
-                    arr[i] = (char) (arr[i] + n - 26);
-                }else{
-                    arr[i] = (char) (arr[i] + n);
-                }
-            }else{
-                if(arr[i] + n > 122){
-                    arr[i] = (char) (arr[i] + n - 26);
-                }else{
-                    arr[i] = (char) (arr[i] + n);
-                }
+            if(arr[i] <= 'Z'){ // 대문자일 경우
+                arr[i] = (char) ((arr[i] - 'A' + n) % 26 + 'A');
+            } else if (arr[i] <= 'z'){ // 소문자일 경우
+                arr[i] = (char) ((arr[i] - 'a' + n) % 26 + 'a');
             }
         }
-        String answer = new String(arr);
-        return answer;
+        return new String(arr);
     }
 }
+// class Solution {
+//     public String solution(String s, int n) {
+//         // 아스키로 간단하게 생각?
+//         // 대문자 65 ~ 90 소문자 97 ~ 122 아스키 범위 정하기
+//         char[] arr = s.toCharArray();
+//         for(int i = 0; i < arr.length; i++){
+//             if(arr[i] == ' ') continue;
+//             if(arr[i] < 91){
+//                 if(arr[i] + n > 90){
+//                     arr[i] = (char) (arr[i] + n - 26);
+//                 }else{
+//                     arr[i] = (char) (arr[i] + n);
+//                 }
+//             }else{
+//                 if(arr[i] + n > 122){
+//                     arr[i] = (char) (arr[i] + n - 26);
+//                 }else{
+//                     arr[i] = (char) (arr[i] + n);
+//                 }
+//             }
+//         }
+//         String answer = new String(arr);
+//         return answer;
+//     }
+// }
