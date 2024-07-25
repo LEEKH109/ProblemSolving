ea46755cc598425b63e024d54fc7914b2b653822
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-07-25 19:19:13 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,19 +1,34 @@
 class Solution {
     boolean solution(String s) {
-        String[] tmp = s.split("");
+        // Ver.2
+        
         int countP = 0;
         int countY = 0;
-        for(String el : tmp){
-            if(el.equals("p") || el.equals("P")){
+        
+        s = s.toLowerCase();
+        for (char ch : s.toCharArray()) {
+            if (ch == 'p') {
                 countP++;
-            } else if(el.equals("y") || el.equals("Y")){
+            } else if (ch == 'y') {
                 countY++;
-            } 
+            }
         }
-        if(countP != countY){
-            return false;
-        }else{
-            return true;
-        }
+        return countP == countY;
+        // Ver.1
+        // String[] tmp = s.split("");
+        // int countP = 0;
+        // int countY = 0;
+        // for(String el : tmp){
+        //     if(el.equals("p") || el.equals("P")){
+        //         countP++;
+        //     } else if(el.equals("y") || el.equals("Y")){
+        //         countY++;
+        //     } 
+        // }
+        // if(countP != countY){
+        //     return false;
+        // }else{
+        //     return true;
+        // }
     }
 }
