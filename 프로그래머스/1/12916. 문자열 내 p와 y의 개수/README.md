856cf9ed97958b2d114600f8feddf3ea0298b826
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-07-27 17:24:30 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
 
 ### 성능 요약
 
-메모리: 70.1 MB, 시간: 0.78 ms
+메모리: 73.7 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 19:30:47
+2024년 07월 27일 17:24:29
 
 ### 문제 설명
 
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,22 +1,40 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 class Solution {
     boolean solution(String s) {
-        int countP = countOccurrences(s, "p");
-        int countY = countOccurrences(s, "y");
-        return countP == countY;
-    }
-    
-    private int countOccurrences(String s, String ch) {
-        Pattern pattern = Pattern.compile(ch, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(s);
-        
+        s = s.toLowerCase();
         int count = 0;
-        while (matcher.find()) {
-            count++;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'p')
+                count++;
+            else if (s.charAt(i) == 'y')
+                count--;
         }
-        
-        return count;
+
+        if (count == 0)
+            return true;
+        else
+            return false;
     }
 }
+// import java.util.*;
+
+// class Solution {
+//     boolean solution(String s) {
+//         int countP = countOccurrences(s, "p");
+//         int countY = countOccurrences(s, "y");
+//         return countP == countY;
+//     }
+    
+//     private int countOccurrences(String s, String ch) {
+//         Pattern pattern = Pattern.compile(ch, Pattern.CASE_INSENSITIVE);
+//         Matcher matcher = pattern.matcher(s);
+        
+//         int count = 0;
+//         while (matcher.find()) {
+//             count++;
+//         }
+        
+//         return count;
+//     }
+// }
