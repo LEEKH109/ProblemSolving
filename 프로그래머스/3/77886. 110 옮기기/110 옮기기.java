70ffface029b993622bf799d42cf220a5b23c8ef
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-01 16:44:09 +0900

    [level 3] Title: 110 옮기기, Time: 127.23 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.java
@@ -4,53 +4,55 @@
 // 음 110이 어디 있는지 찾는게 1번
 // 110이 여러개 일 대도 커버가 되는게 2번   
 // 110을 체크하고 전부 제거, 그리고 갯수 카운트
-// 못 풀겠어서 블로그 봤어요....
+// 문제 예시에서 주어진 예제들을 보면 문자열에 0 이 있을 때는 가장 마지막 0 뒤에 110을 달아주면 되고
+// 0이 없으면 시작 부분에 110 달아주면 된다.
+// 3600ms 나오는데 다른 사람들은?
 
 import java.util.*;
 
 class Solution {
     public String[] solution(String[] s) {
-        String[] answer = new String[s.length];
-        
-        for(int i=0; i<s.length; i++){
-            String ans = solve(s[i]);   
-            answer[i] = ans;
+        int n = s.length;
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            int m = s[i].length();
+            List<Character> a = new ArrayList<>();
+            int c = 0;
+            for (int j = 0; j < m; j++) {
+                a.add(s[i].charAt(j));
+                int l = a.size();
+                while (l > 2 && a.get(l - 3) == '1' && a.get(l - 2) == '1' && a.get(l - 1) == '0') {
+                    c++;
+                    a.remove(l - 1);
+                    a.remove(l - 2);
+                    a.remove(l - 3);
+                    l -= 3;
+                }
+            }
+            if (c > 0) {
+                char [] x = new char[m];
+                int o = 0;
+                int r = a.size();
+                while (r > 0 && a.get(r - 1) == '1') {
+                    r--;
+                }
+                for (int j = 0; j < r; j++) {
+                    x[o++] = a.get(j);
+                }
+                for (int j = 0; j < c; j++) {
+                    x[o++] = '1';
+                    x[o++] = '1';
+                    x[o++] = '0';
+                }
+                while (r < a.size()) {
+                    x[o++] = a.get(r++);
+                }
+                answer[i] = new String(x);
+            } else {
+                answer[i] = s[i];
+            }
         }
-        
         return answer;
     }
-    
-    public String solve(String s){
-        StringBuilder sb = new StringBuilder();// 110 제거된 문자열 담을
-        int count = 0;// 110 갯수 저장 할 변수
-        
-        for(int i=0; i<s.length(); i++){
-            Character c = s.charAt(i);
-            if(sb.length()>=2 && c=='0' && sb.charAt(sb.length()-2)=='1' && sb.charAt(sb.length()-1)=='1'){
-                count++;
-                sb.delete(sb.length()-2, sb.length());
-            }
-            else{
-                sb.append(c);
-            }
-        }
-        
-        if(count > 0){
-            if(sb.indexOf("0")==-1){
-                // '0'이 없으면 sb의 시작 부분에 "110"을 삽입
-                for(int i = 0; i < count; i++) {
-                    sb.insert(0, "110");
-                }
-            }
-            else{
-                // 마지막 '0' 뒤에 "110"을 삽입
-                int idx = sb.lastIndexOf("0");
-                for(int i = 0; i < count; i++) {
-                    sb.insert(idx + 1, "110");
-                }
-            }
-        }
-        
-        return sb.toString();
-    }
 }
+
