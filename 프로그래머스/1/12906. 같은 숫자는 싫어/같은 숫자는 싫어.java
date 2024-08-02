0af0863a038a4638bb1a6fe0a83b0b196be9b3e4
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-02 12:48:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.72 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,27 +1,21 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        // 스택이 비어 있다면? 배열 해당 숫자 스택에 넣어 주기
-        // 스택에 숫자가 있다면? 지금 숫자와 동일한 숫자이면 continue
-        // 스택에 숫자가 있는데 동일한 숫자가 아니다? 스택에 있는 숫자 꺼내주면서 answer에 넣어주고
-        // 지금 숫자 다시 스택에 넣어주기
-        Stack<Integer>stack = new Stack<>();
+    public int[] solution(int[] arr) {
+        if (arr.length == 0) return new int[0];  // 빈 배열 처리
+        
         ArrayList<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < arr.length; i++){
-            if(stack.isEmpty()){
-                stack.push(arr[i]);
-            }else if(stack.peek() == arr[i]){
-                continue;
-            }else if(stack.peek() != arr[i]){
-                answer.add(stack.pop());
-                stack.push(arr[i]);
+        int pre = arr[0];
+        answer.add(pre);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (pre != arr[i]) {
+                answer.add(arr[i]);
+                pre = arr[i];
             }
         }
-        while (!stack.isEmpty()) {
-            answer.add(stack.pop());
-        }
         
+        // ArrayList를 int 배열로 변환
         int[] result = new int[answer.size()];
         for (int i = 0; i < answer.size(); i++) {
             result[i] = answer.get(i);
