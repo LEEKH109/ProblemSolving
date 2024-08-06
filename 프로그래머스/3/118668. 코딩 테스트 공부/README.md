49c51bc37188861305653b03edbb9739673638ad
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-06 17:31:53 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 51.84 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/README.md
+++ 프로그래머스/3/118668. 코딩 테스트 공부/README.md
@@ -1,10 +1,10 @@
 # [level 3] 코딩 테스트 공부 - 118668 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118668) 
 
 ### 성능 요약
 
-메모리: 52.5 MB, 시간: 38.11 ms
+메모리: 57.9 MB, 시간: 51.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 17:18:36
+2024년 08월 06일 17:31:51
 
 ### 문제 설명
 
--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,65 +1,72 @@
 import java.util.*;
- 
+class Node{
+    int alp, cop, cost;
+    Node(int alp, int cop, int cost){
+        this.alp = alp;
+        this.cop = cop;
+        this.cost = cost;
+    }
+}
 class Solution {
     public int solution(int alp, int cop, int[][] problems) {
-            
-         int goal_a=0;
-         int goal_c=0;
-        //목표치를 구하는 for문
-        for(int i =0; i<problems.length;i++){
-            goal_a=Math.max(problems[i][0],goal_a);
-            goal_c=Math.max(problems[i][1],goal_c);
+        int answer = 0;
+        int max_alp = Integer.MIN_VALUE, max_cop = Integer.MIN_VALUE;
+        int[][] v = new int[1000][1000];
+
+        for(int i=0; i<problems.length; i++){
+            if(max_alp<problems[i][0]){
+                max_alp = problems[i][0];
+            }
+            if(max_cop<problems[i][1]){
+                max_cop = problems[i][1];
+            }
         }
-        
-        if(goal_a<=alp&&goal_c<=cop){
-            return 0;
+
+        for(int i=0; i<v.length; i++){
+            for(int j=0; j<v[0].length; j++){
+                v[i][j] = Integer.MAX_VALUE;
+            }
         }
-        
-        if(alp>=goal_a){
-            alp=goal_a;
-        }
-        if(cop>=goal_c){
-            cop=goal_c;
-        }
-        
-        int[][] dp =new int[goal_a+2][goal_c+2];
-        
-        for(int i=alp;i<=goal_a;i++){
-            for(int j=cop;j<=goal_c;j++){
-                dp[i][j]=Integer.MAX_VALUE;
-          }
-        }
-        
-        dp[alp][cop]=0;
-        
-         for(int i=alp;i<=goal_a;i++){
-            for(int j=cop;j<=goal_c;j++){
-                
-                dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+1);
-                
-                dp[i][j+1]=Math.min(dp[i][j+1],dp[i][j]+1);
-                
-                for(int[] p :problems){
-    
-                if(i>=p[0]&&j>=p[1]){
-                    if(i+p[2]>goal_a&&j+p[3]>goal_c){
-                      dp[goal_a][goal_c]=Math.min(dp[goal_a][goal_c],dp[i][j]+p[4]);
-                    }
-                    else if(i+p[2]>goal_a){
-                        dp[goal_a][j+p[3]]=Math.min(dp[goal_a][j+p[3]],dp[i][j]+p[4]);
-                    }
-                    else if(j+p[3]>goal_c){
-                        dp[i+p[2]][goal_c]=Math.min(dp[i+p[2]][goal_c],dp[i][j]+p[4]);
-                    }
-                    else if(i+p[2]<=goal_a&&j+p[3]<=goal_c){
-                       dp[i+p[2]][j+p[3]]=Math.min(dp[i+p[2]][j+p[3]],dp[i][j]+p[4]); 
-                    }
+
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(new Comparator<Node>(){
+            @Override
+            public int compare(Node n1, Node n2){
+                if(n1.cost<n2.cost){
+                    return -1;
+                }else if(n1.cost>n2.cost){
+                    return 1;
+                }else{
+                    return 0;
                 }
-                                  
-                 }
+            }
+        });
+
+        pq.add(new Node(alp,cop,0));
+        while(!pq.isEmpty()){
+            Node n = pq.poll();
+            if(n.alp >= max_alp && n.cop >= max_cop){
+                return n.cost;
+            }
+            if(v[n.alp+1][n.cop]>n.cost+1){
+                pq.add(new Node(n.alp+1,n.cop,n.cost+1));
+                v[n.alp+1][n.cop] = n.cost+1;
+            }
+
+            if(v[n.alp][n.cop+1]>n.cost+1){
+                pq.add(new Node(n.alp,n.cop+1,n.cost+1));
+                v[n.alp][n.cop+1] = n.cost+1;
+            }
+
+
+            for(int i=0; i<problems.length; i++){
+                if(n.alp>=problems[i][0]&&n.cop>=problems[i][1]&&v[n.alp+problems[i][2]][n.cop+problems[i][3]]>n.cost+problems[i][4]){
+                    pq.add(new Node(n.alp+problems[i][2],n.cop+problems[i][3],n.cost+problems[i][4]));
+                    v[n.alp+problems[i][2]][n.cop+problems[i][3]]=n.cost+problems[i][4];
                 }
-          }
-        
-        return dp[goal_a][goal_c];
+            }
+        }
+
+
+        return answer;
     }
 }
