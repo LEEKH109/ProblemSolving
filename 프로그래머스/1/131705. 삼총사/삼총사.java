c2382408f6a1699a2e2bf0a2910b74f06daa3e01
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-16 13:20:41 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,32 @@
 class Solution {
-    static int answer;
     public int solution(int[] number) {
-        answer = 0;
-        dfs(number, 0, 0, 0);
-        return answer;
-    }
-    public void dfs(int[] number, int index, int sum, int count){
-        if(count == 3 && sum == 0){
-            answer ++;
-        }else{
-            for(int i = index; i < number.length; i++){
-                dfs(number, i+1, sum+number[i], count+1);
+        int answer = 0;
+
+        for(int i=0; i<number.length; i++){
+            for(int j=i+1; j<number.length; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k]==0) answer++;
+                }
             }
         }
+
+        return answer;
     }
 }
+// class Solution {
+//     static int answer;
+//     public int solution(int[] number) {
+//         answer = 0;
+//         dfs(number, 0, 0, 0);
+//         return answer;
+//     }
+//     public void dfs(int[] number, int index, int sum, int count){
+//         if(count == 3 && sum == 0){
+//             answer ++;
+//         }else{
+//             for(int i = index; i < number.length; i++){
+//                 dfs(number, i+1, sum+number[i], count+1);
+//             }
+//         }
+//     }
+// }
