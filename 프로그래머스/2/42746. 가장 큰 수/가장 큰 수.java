ffe779847dd66abaac82cf1a89e637de66641d9a
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-16 13:50:06 +0900

    [level 2] Title: 가장 큰 수, Time: 3345.34 ms, Memory: 406 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,27 +1,40 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        Integer[] numArray = new Integer[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            numArray[i] = numbers[i];
-        }
-
-        Arrays.sort(numArray, (a, b) -> {
-            int ab = Integer.parseInt("" + a + b);
-            int ba = Integer.parseInt("" + b + a);
-            return Integer.compare(ba, ab);
-        });
-
-        if (numArray[0] == 0) {
-            return "0";
-        }
-
-        StringBuilder answer = new StringBuilder();
-        for (int num : numArray) {
-            answer.append(num);
-        }
-
-        return answer.toString();
+        String answer = "";
+        answer = Arrays.stream(numbers)
+        .mapToObj(String::valueOf)
+        .sorted((s1, s2) -> -s1.concat(s2).compareTo(s2.concat(s1)))
+        .reduce("", (s1, s2) -> s1.equals("0") && s2.equals("0") ? "0" : s1.concat(s2));
+        return answer;
     }
 }
+// import java.util.*;
+
+// class Solution {
+//     public String solution(int[] numbers) {
+//         Integer[] numArray = new Integer[numbers.length];
+//         for (int i = 0; i < numbers.length; i++) {
+//             numArray[i] = numbers[i];
+//         }
+
+//         Arrays.sort(numArray, (a, b) -> {
+//             int ab = Integer.parseInt("" + a + b);
+//             int ba = Integer.parseInt("" + b + a);
+//             return Integer.compare(ba, ab);
+//         });
+
+//         if (numArray[0] == 0) {
+//             return "0";
+//         }
+
+//         StringBuilder answer = new StringBuilder();
+//         for (int num : numArray) {
+//             answer.append(num);
+//         }
+
+//         return answer.toString();
+//     }
+// }
