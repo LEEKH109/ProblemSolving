ff2afe64f65a2c18724c58f30d81088645d7038f
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-19 14:35:03 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.15 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int l = Math.max(n,m);
-        int r = Math.min(n,m);
-        int a = 1;
-        while(a < l){
-            if(l%a == 0 && r%a == 0){
-                answer[0] = a;
+        // 내가 푼 풀이
+        // int l = Math.max(n,m);
+        // int r = Math.min(n,m);
+        // int a = 1;
+        // while(a < l){
+        //     if(l%a == 0 && r%a == 0){
+        //         answer[0] = a;
+        //     }
+        //     a++;
+        // }
+        // int b = r;
+        // while(true){
+        //     if(b%l == 0 && b%r == 0){
+        //         answer[1] = b;
+        //         break;
+        //     }
+        //     b++;
+        // }
+        // 좋은 풀이
+        for (int i = 1; i < n + m; i++) {
+            if (n % i == 0 && m % i == 0) {
+                answer[0] = i;
+                answer[1] = n * m / answer[0];
             }
-            a++;
-        }
-        int b = r;
-        while(true){
-            if(b%l == 0 && b%r == 0){
-                answer[1] = b;
-                break;
-            }
-            b++;
         }
         return answer;
     }
