d809db3f2bfd8d8e3d9418cfd8a7e8785b8405d8
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-08-25 20:29:18 +0900

    [level 2] Title: 광물 캐기, Time: 12.28 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,3 +1,13 @@
+// 문제: 172927번 (광물 캐기)
+// 등급: Level 2
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/172927
+// [풀이]
+// 곡괭이를 한 번 고르면 5개 부술 때까지 계속 사용
+// 사용 할 수 있는 곡괭이의 갯수는 문제 초반에 지정
+// 정지 조건 모든 광물 캐기 or 곡괭이 다 쓰기
+// 완탐을 할 수 밖에 없는 문제라고 생각, picks가 3종류 이고 미네랄이 최대 50개라 곡괭이를 최대 10개 사용 가능
+// => DFS시 3^10 =~ 5만 
+// 2차 코드
 class Solution {
     int answer;
     int[][] tool = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
@@ -5,14 +15,14 @@ class Solution {
 
     public int solution(int[] picks, String[] minerals) {
         answer = 25 * 50 + 1;
-        remainingTools = picks[0] + picks[1] + picks[2]; // 초기 곡괭이 수를 계산
+        remainingTools = picks[0] + picks[1] + picks[2];
         for (int i = 0; i < 3; i++) {
             if (picks[i] > 0) {
                 picks[i]--;
-                remainingTools--; // 곡괭이 사용
+                remainingTools--;
                 DFS(picks, minerals, i, 0, 0);
                 picks[i]++;
-                remainingTools++; // 곡괭이 복원
+                remainingTools++;
             }
         }
         return answer;
@@ -31,20 +41,62 @@ class Solution {
             cnt++;
             mineralIdx++;
         }
-
         if (remainingTools == 0 || mineralIdx == minerals.length) {
             answer = Math.min(answer, sum);
-            return; // 더 이상 진행할 곡괭이가 없거나 광물을 모두 캘 경우 종료
+            return;
         }
-
         for (int i = 0; i < 3; i++) {
             if (picks[i] > 0) {
                 picks[i]--;
-                remainingTools--; // 곡괭이 사용
+                remainingTools--;
                 DFS(picks, minerals, i, mineralIdx, sum);
                 picks[i]++;
-                remainingTools++; // 곡괭이 복원
+                remainingTools++;
             }
         }
     }
 }
+// 1차 코드
+// class Solution {
+//     int answer;
+//     int[][] tool = {{1, 1, 1},{5, 1, 1},{25, 5, 1}};
+//     public int solution(int[] picks, String[] minerals) {
+//         answer = 25*50+1;
+//         for(int i = 0; i < 3; i++){
+//             if(picks[i] != 0){
+//                 picks[i]--;
+//                 DFS(picks, minerals, i, 0, 0);
+//                 picks[i]++;
+//             }
+//         }
+//         return answer;
+//     }
+//     public void DFS(int[] picks, String[] minerals, int pickIdx, int mineralIdx, int sum){
+//         int cnt = 0;
+//         while(cnt < 5 && mineralIdx < minerals.length){
+//             if(minerals[mineralIdx].equals("diamond")){
+//                 sum += tool[pickIdx][0];
+//             }else if(minerals[mineralIdx].equals("iron")){
+//                 sum += tool[pickIdx][1];
+//             }else{
+//                 sum += tool[pickIdx][2];
+//             }
+//             cnt++;
+//             mineralIdx++;
+//         }
+//         int tmp = 0;
+//         for(int el : picks){
+//             tmp += el;
+//         }
+//         if(tmp == 0 || mineralIdx == minerals.length){
+//             answer = Math.min(answer, sum);
+//         }
+//         for(int i = 0; i < 3; i++){
+//             if(picks[i] != 0){
+//                 picks[i]--;
+//                 DFS(picks, minerals, i, mineralIdx, sum);
+//                 picks[i]++;
+//             }
+//         }
+//     }
+// }
