97f2abfbda24396c50a39ff83df26c5ef33066ca
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-10-30 13:03:50 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.48 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        Arrays.sort(A); // A 배열 오름차순 정렬
-        Arrays.sort(B); // B 배열 오름차순 정렬
-
-        // B 배열 내림차순으로 뒤집기
-        for (int i = 0; i < B.length / 2; i++) {
-            int temp = B[i];
-            B[i] = B[B.length - 1 - i];
-            B[B.length - 1 - i] = temp;
-        }
-
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int n = A.length-1;
         int answer = 0;
-        for (int i = 0; i < A.length; i++) {
-            answer += A[i] * B[i];
+        for (int i = 0; i <= n; i++) {
+            answer += A[i] * B[n-i];
         }
         return answer;
     }
