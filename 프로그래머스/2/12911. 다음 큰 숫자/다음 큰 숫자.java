b943817960037bd8f2432161582f7813ee3e7c18
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-10-30 13:57:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,13 +1,23 @@
+// class Solution {
+//     public int solution(int n) {
+//         int targetCount = Integer.bitCount(n); // n의 1의 개수를 구함
+//         int answer = n + 1; // n보다 큰 수부터 탐색 시작
+
+//         // n보다 큰 숫자 중에서 1의 개수가 같은 숫자를 찾음
+//         while (Integer.bitCount(answer) != targetCount) {
+//             answer++;
+//         }
+
+//         return answer;
+//     }
+// }
 class Solution {
     public int solution(int n) {
-        int targetCount = Integer.bitCount(n); // n의 1의 개수를 구함
-        int answer = n + 1; // n보다 큰 수부터 탐색 시작
+        int smallest = n & -n;         // 가장 오른쪽 1을 찾음
+        int ripple = n + smallest;     // 오른쪽의 0을 1로 변경해 다음 큰 수 패턴 생성
+        int ones = n ^ ripple;         // 원래 숫자와의 XOR 연산으로 바뀐 비트 추출
+        ones = (ones >> 2) / smallest; // ones를 오른쪽으로 정렬
 
-        // n보다 큰 숫자 중에서 1의 개수가 같은 숫자를 찾음
-        while (Integer.bitCount(answer) != targetCount) {
-            answer++;
-        }
-
-        return answer;
+        return ripple | ones;          // 새로운 패턴을 합쳐서 다음 큰 수 반환
     }
 }
