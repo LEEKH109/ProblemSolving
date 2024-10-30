d083412a4fde608765f7a2426cea8d31689b8312
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-10-30 13:31:02 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,17 +1,31 @@
+// class Solution {
+//     public int solution(int n) {
+//         int cnt = 0;
+//         for(int i = 1; i <= n; i++){
+//             int sum = 0;
+//             for(int j = i; j <= n; j++){
+//                 if(sum+j > n){
+//                     break;
+//                 }else if(sum+j == n){
+//                     cnt++;
+//                     break;
+//                 }else if(sum+j < n){
+//                     sum+=j;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// }
 class Solution {
     public int solution(int n) {
         int cnt = 0;
+        int sum = 0;
         for(int i = 1; i <= n; i++){
-            int sum = 0;
-            for(int j = i; j <= n; j++){
-                if(sum+j > n){
-                    break;
-                }else if(sum+j == n){
-                    cnt++;
-                    break;
-                }else if(sum+j < n){
-                    sum+=j;
-                }
+            sum += i;
+            if(n-sum < 0) break;
+            if((n-sum)%i == 0){
+                cnt++;
             }
         }
         return cnt;
