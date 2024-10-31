5caf1090e33f0c68b5f7e02f54fa57716cdc42e8
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-10-31 13:44:22 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,32 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int area = brown + yellow;
-        int[] answer = new int[2];
-        int y = 0;
-        for(int x = 1; x <= area; x++){
-            if(area % x != 0) continue;
-            y = area / x;
-            if(2*x+2*y-4 == brown){
-                answer[0] = Math.max(x,y);
-                answer[1] = Math.min(x,y);
-                break;
-            }
-        }
-        return answer;
+        // 2x+2y-4 = brown; => x+y = (brown+4)/2
+        // x-y = Math.sqrt((x+y)*(x+y) - 4*area);
+        // 2x = (brown+4)/2 + Math.sqrt((x+y)*(x+y) - 4*area);
+        int x = (int)(((brown + 4) / 2.0 + Math.sqrt((brown + 4) / 2.0 * (brown + 4) / 2.0 - 4 * area)) / 2);
+        return new int[] {Math.max(x, area / x), Math.min(x, area / x)};
     }
 }
+
+// 초안
+// import java.util.*;
+// class Solution {
+//     public int[] solution(int brown, int yellow) {
+//         int area = brown + yellow;
+//         int[] answer = new int[2];
+//         int y = 0;
+//         for(int x = 1; x <= area; x++){
+//             if(area % x != 0) continue;
+//             y = area / x;
+//             if(2*x+2*y-4 == brown){
+//                 answer[0] = Math.max(x,y);
+//                 answer[1] = Math.min(x,y);
+//                 break;
+//             }
+//         }
+//         return answer;
+//     }
+// }
 // xy 면적이 주어지고
 // 주어진 값을 만족하는 x 와 y를 각각 구하는 문제
