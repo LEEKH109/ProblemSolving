a04380639c956ca29f1836aec7c0d2fe746267c5
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-10-31 14:51:21 +0900

    [level 2] Title: 구명보트, Time: 13.64 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 54.1 MB, 시간: 10.28 ms
+메모리: 56.8 MB, 시간: 13.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 24일 22:33:07
+2024년 10월 31일 14:51:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,25 +1,14 @@
-// 문제: 42885번 (구명보트)
-// 등급: Level 2
-// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42885
-// [풀이] 
-// 구명보트의 무게 제한은 항상 사람들의 몸무게 중 최댓값보다 크게 주어진다. 혼자서 꽉 차는 경우는 없다.
-// 구명보트는 최대 2명까지
-// 50000 * 50000 = 25억 => 완탐 어려움, 투 포인터 값 비교
 import java.util.*;
+// 최대 2명까지라고 주어져서 쉬운 문제, 문제를 잘 읽자
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people); 
-        int minIdx = 0;
-        int maxIdx = people.length - 1;
-        int answer = 0;
-        // 정지 조건 => 교차
-        while (maxIdx >= minIdx) {
-            if (people[maxIdx] + people[minIdx] <= limit) {
-                minIdx++;
-            }
-            maxIdx--;
-            answer++;
+        int cnt = 0;
+        Arrays.sort(people);
+        int n = people.length, s = 0, e = n-1;
+        while(s <= e){
+            if(people[s] + people[e] <= limit) s++;
+            e--; cnt++;
         }
-        return answer;
+        return cnt;
     }
 }
