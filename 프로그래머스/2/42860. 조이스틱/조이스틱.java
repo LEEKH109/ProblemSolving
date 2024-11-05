15e8d27fadf98d182a0fe970a1c44993f06f3863
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-11-05 16:54:28 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,40 +1,30 @@
-import java.util.*;
 class Solution {
+    // "BBABAAABAABB" => 11
+    // "BBBBAAAABA" => 12
     public int solution(String name) {
         int n = name.length();
-        boolean[] visited = new boolean[n];
-        visited[0] = name.charAt(0) == 'A';
-        return dfs(0, 0, visited, name);
-    }
-
-    public int dfs(int nowIdx, int cnt, boolean[] visited, String name) {
-        boolean toggle = true;
-        int n = name.length();
-        int minCnt = 987654321;
+        int cnt = 0;
+        int minMove = n - 1;
 
         for (int i = 0; i < n; i++) {
-            if (name.charAt(i) != 'A' && !visited[i]) {
-                toggle = false;
-                int moveCnt = move(nowIdx, i, n);
-                int swapCnt = swap(name.charAt(i));
-                visited[i] = true;
-                int totalCnt = dfs(i, cnt + moveCnt + swapCnt, visited, name);
-                minCnt = Math.min(minCnt, totalCnt);
-                visited[i] = false;
+            char tmp = name.charAt(i);
+            cnt += swap(tmp);
+
+            int idx = i + 1;
+            while (idx < n && name.charAt(idx) == 'A') {
+                idx++;
             }
+            int moveRL = i + i + n - idx;
+            int moveLR = (n - idx) + (n - idx) + i;
+            int move = Math.min(moveRL, moveLR);
+            minMove = Math.min(minMove, move);
         }
-        if (toggle) {
-            return cnt;
-        } else {
-            return minCnt;
-        }
+
+        cnt += minMove;
+        return cnt;
     }
-    
-    public int swap(char target){
-        return Math.min('Z'-target+1, target-'A');
-    }
-    
-    public int move(int s, int e, int n){
-        return Math.min((e - s + n) % n, (s - e + n) % n);
+
+    public int swap(char target) {
+        return Math.min(target - 'A', 'Z' - target + 1);
     }
 }
