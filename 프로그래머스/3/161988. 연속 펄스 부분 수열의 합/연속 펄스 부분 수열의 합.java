89f50045734301b3a8be25fcc2d0893b60a8a323
LEEKH109 <108852263+LEEKH109@users.noreply.github.com>
2024-11-21 16:51:19 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 19.45 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,34 +1,33 @@
 class Solution {
     public long solution(int[] sequence) {
         int n = sequence.length;
-        long[] plus = new long[n];
-        long[] minus = new long[n];
+        int[] plus = new int[n];
+        int[] minus = new int[n];
 
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                plus[i] = sequence[i] * 1L;
-                minus[i] = sequence[i] * -1L;
+                plus[i] = sequence[i];
+                minus[i] = sequence[i] * -1;
             } else {
-                plus[i] = sequence[i] * -1L;
-                minus[i] = sequence[i] * 1L;
+                plus[i] = sequence[i] * -1;
+                minus[i] = sequence[i];
             }
         }
 
-        long maxPlus = kadane(plus);
-        long maxMinus = kadane(minus);
+        long maxPlus = sum(plus);
+        long maxMinus = sum(minus);
 
         return Math.max(maxPlus, maxMinus);
     }
 
-    private long kadane(long[] array) {
-        long maxSum = array[0];
-        long currentSum = array[0];
+    private long sum(int[] arr) {
+        long maxSum = arr[0];
+        long currentSum = arr[0];
 
-        for (int i = 1; i < array.length; i++) {
-            currentSum = Math.max(array[i], currentSum + array[i]);
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
             maxSum = Math.max(maxSum, currentSum);
         }
-
         return maxSum;
     }
 }
